e2416705104b9826d0d0c45ec420183e35b293fa
Umidjon <makhammadsoliyev@gmail.com>
2024-04-18 16:36:06 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Org.BouncyCastle.Crypto.Digests;
+using System.Text;
+
+
+var filesPath = @"..\..\..\..\SHA3-256\Files\";
+var hashList = CalculateAllSHA3FromFiles(filesPath);
+
+hashList.Sort();
+
+var concatenatedHashes = string.Join("", hashList);
+var email = "makhammadsoliyev@gmail.com";
+
+concatenatedHashes += email;
+
+var result = CalculateSHA3(Encoding.UTF8.GetBytes(concatenatedHashes));
+Console.WriteLine(result);
+
+static List<string> CalculateAllSHA3FromFiles(string filesPath)
+{
+    var hashList = new List<string>();
+    var files = Directory.GetFiles(filesPath);
+
+    foreach (var file in files)
+    {
+        var fileContent = File.ReadAllBytes(file);
+        var hash = CalculateSHA3(fileContent);
+
+        hashList.Add(hash);
+    }
+
+    return hashList;
+}
+
+static string CalculateSHA3(byte[] data)
+{
+    var sha3 = new Sha3Digest(256);
+
+    var hasBytes = new byte[sha3.GetDigestSize()];
+
+    sha3.BlockUpdate(data, 0, data.Length);
+
+    sha3.DoFinal(hasBytes, 0);
+
+    var hashBuilder = new StringBuilder();
+
+    foreach ( var b in hasBytes )
+        hashBuilder.Append(b.ToString("x2"));
+
+    return hashBuilder.ToString();
+}
